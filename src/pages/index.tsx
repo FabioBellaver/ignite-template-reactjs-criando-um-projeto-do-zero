079e990f97e9caa406da464ee5237de7b03c2de8
Fábio Bellaver <fabio.bellaver658@gmail.com>
2022-05-21 20:18:31 -0300

    home page done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import { GetStaticProps } from 'next';
-
+import Header from '../components/Header';
 import { getPrismicClient } from '../services/prismic';
-
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { FiCalendar, FiUser } from 'react-icons/fi'
+import Link from 'next/link'
 
 interface Post {
   uid?: string;
@@ -15,7 +16,7 @@ interface Post {
   };
 }
 
-/*
+
 interface PostPagination {
   next_page: string;
   results: Post[];
@@ -25,13 +26,48 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
- export default function Home() {
-   <h1>Hello world</h1>
- }
+export default function Home() {
+  return (
+    <>
+      <Header />
 
- export const getStaticProps = async () => {
-    const prismic = getPrismicClient({});
-    const postsResponse = await prismic.getByType(//TODO);
+      <main className={commonStyles.container}>
+        <Link href="#">
+          <a>
+            <div className={styles.content}>
+              <h1>Como utilizar Hooks</h1>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div className={styles.info}>
+                <time><FiCalendar />15 Mar 2021</time><span><FiUser />Fábio Bellaver</span>
+              </div>
+            </div>
+          </a>
+        </Link>
+        <Link href="#">
+          <a>
+            <div className={styles.content}>
+              <h1>Como utilizar Hooks</h1>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div className={styles.info}>
+                <time><FiCalendar />15 Mar 2021</time><span><FiUser />Fábio Bellaver</span>
+              </div>
+            </div>
+          </a>
+        </Link>
+
+      <button 
+      type="button"
+      className={styles.btn}
+      >Carregar mais posts</button>
+
+      </main>
+    </>
+  );
+}
+
+// export const getStaticProps = async () => {
+//    const prismic = getPrismicClient({});
+//    const postsResponse = await prismic.getByType(//TODO);
 
    // TODO
- };*/
+// };
